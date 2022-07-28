f9fd9334f60f2134d8fdd8fb3d9074be1773ebd1
HenLevi <heni8500@gmail.com>
2022-07-28 09:25:54 +0300

    Update AssetsDao.java

--- walth-rating-ex-hen-levi/src/main/java/com/microservices/dao/AssetsDao.java
+++ walth-rating-ex-hen-levi/src/main/java/com/microservices/dao/AssetsDao.java
@@ -2,7 +2,7 @@ package com.microservices.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.microservices.model.AssetPerCity;
-
+@Repository
 public interface AssetsDao extends JpaRepository<AssetPerCity,Integer> {
 
 }
